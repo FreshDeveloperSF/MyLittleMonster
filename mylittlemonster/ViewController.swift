31d7285bdf5cc78ac67622034f2df43d68027256
Jason Leung <jasonleung@jasons-macbook-pro-3.local>
2016-05-23 15:22:14 -0700

    Added DragImg class for heart and food graphics

--- mylittlemonster/ViewController.swift
+++ mylittlemonster/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var monsterImg: UIImageView!
+    @IBOutlet weak var foodImg: DragImg!
+    @IBOutlet weak var heartImg: DragImg!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +30,8 @@ class ViewController: UIViewController {
         monsterImg.startAnimating()
         
     }
+    
+    
 
 
 }
