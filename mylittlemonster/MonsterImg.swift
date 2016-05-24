79006845bc4f3468bb37600939791189555cc60f
Jason Leung <jasonleung@jasons-macbook-pro-3.local>
2016-05-24 15:33:37 -0700

    Fixed bug for animationRepeatCount

--- mylittlemonster/MonsterImg.swift
+++ mylittlemonster/MonsterImg.swift
@@ -52,7 +52,7 @@ class MonsterImg: UIImageView {
         
         self.animationImages = imgArray
         self.animationDuration = 0.8
-        self.animationRepeatCount = 0
+        self.animationRepeatCount = 1
         self.startAnimating()
     }
 }
